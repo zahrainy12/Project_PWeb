85ec91a94306e9b65b09187f7cfbbfbae0fa0d85
Anja Bunga Aditya <bungabunga0603@gmail.com>
2024-06-20 17:56:23 +0700

    ini folder login & sign up mahasiswa

---
+++ mahasiswa/login_mhs.php
@@ -0,0 +1,23 @@
+<?php
+include("koneksi.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emailLogin = $_POST['emailLogin'];
+    $passwordLogin = $_POST['passwordLogin'];
+
+    $query = "SELECT * FROM akun_mahasiswa WHERE email='$emailLogin' AND password='$passwordLogin'";
+    $result = mysqli_query($link, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        // Login berhasil
+        header("Location: index.php?login=success");
+        exit();
+    } else {
+        // Login gagal
+        header("Location: index.php?login=failed");
+        exit();
+    }
+}
+
+mysqli_close($link);
+?>
